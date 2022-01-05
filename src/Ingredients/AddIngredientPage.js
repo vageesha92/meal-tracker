bf40b24ac2c8a7b7036926bd15365a1717d380ea
Vageesha Singh <vageesha92@gmail.com>
2022-01-05 17:57:03 -0600

    Add react components for Recipe Search, Shopping List and Adding Ingredients
    They have only display simple heading right now.

---
+++ src/Ingredients/AddIngredientPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AddIngredient=()=>(
+<h1>Add Ingredient for recipe</h1>
+);
---
+++ src/Recipes/RecipeSearch.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const RecipeSearchPage=()=>(
+<h1>This is the Search Recipe Page</h1>
+);
