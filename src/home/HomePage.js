4c2ef252fffdac8075325ea5e0c63ffb95697d2a
Vageesha Singh <vageesha92@gmail.com>
2022-01-06 18:30:24 -0600

    in console print meals from backend

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { useMeals } from "../Meals";
 
-export const HomePage =()=>(
+export const HomePage =()=> {
+    const {meals, isLoading: isLoadingMeals, setMeals}=useMeals();
+    console.log("Meals:");
+    console.log(meals);
+    return (
     <h1>This is the Home Page</h1>
 );
+}
