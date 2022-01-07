0ed5adbf22059566238addbf250c7a5bcf7ea387
Vageesha Singh <vageesha92@gmail.com>
2022-01-06 19:30:48 -0600

    Creating useMeals hook and checking if its loading

---
+++ src/Meals/useMeals.js
@@ -0,0 +1,22 @@
+import {useState, useEffect} from "react";
+
+export const useMeals=()=>{
+    const[isLoading, setisLoading]= useState(true);
+    const[rawMeals,setRawMeals]=useState([]);
+    useEffect(()=>{
+           const loadMeals=async()=>{
+               const response=await fetch("/meals");
+               const rawMealsResponse=await response.json();
+               setRawMeals(meals);
+               setisLoading(false);
+           }
+           loadMeals();
+    },[]);
+   return{isLoading, 
+    meals: rawMeals.map(rawMeal=>({
+        ...rawMeal,
+        PlannedDate: new Date (rawMeal.PlannedDate),
+    })), 
+    setMeals: setRawMeals };
+}
+
