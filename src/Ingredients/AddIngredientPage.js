74082ad58c8b30229f955bd794606a5110edb2e6
Vageesha Singh <vageesha92@gmail.com>
2022-01-07 17:31:09 -0600

    Add component to add ingredient

--- src/Ingredients/AddIngredientPage.js
+++ src/Ingredients/AddIngredientPage.js
@@ -1,5 +1,64 @@
-import React from "react";
+import React,{useState} from "react";
+import { BackButton, DropDown } from "../UI";
+import { useHistory } from "react-router-dom";
 
-export const AddIngredient=()=>(
-<h1>Add Ingredient for recipe</h1>
+const unitOptions=["pounds",
+"cups",
+"count"
+];
+
+
+
+export const AddIngredient=()=> {
+      const [name,setName]=useState("");
+      const[amount,setAmount]=useState(0);
+      const history = useHistory();
+      const[units,setUnits]=useState(unitOptions[0]);   
+       
+      const addToIngredients= async()=>{
+        const newIngredient={
+            name: name.toLowerCase(),
+            amount,
+            units,
+            
+        }
+        await fetch ("/ingredients",{
+            method: "post",
+            body: JSON.stringify(newIngredient),
+            headers:{
+                "Content-Type": "application/json",
+
+            },
+        });
+       history.push("/");
+    }
+    return(
+      <div className="page">
+          <BackButton>
+              <div className="centered-container">
+                  Add Ingredient
+                  <input type="text"
+                  placeholder="Enter ingredient name here"
+                className="space-after space-before full-width"
+                  value={name}
+                  onChange={e=>setName(e.target.value)}/>
+                  <input type="number"
+                  className="space-before full-width"
+                  value={amount}
+                  onChange={e=>setAmount(e.target.value)} />
+                  <DropDown 
+                  className="space-before full-width"
+                  value={units}
+                  onChange={e=>setUnits(e.target.value)}
+                  options={unitOptions}/>
+                  <button 
+                  className="spece-before full-width"
+                  onClick={addToIngredients} >
+                     Add </button>
+              </div>
+          </BackButton>
+      </div>
+
+
 );
+    }
